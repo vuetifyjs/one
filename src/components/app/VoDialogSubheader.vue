a18143912fa638ae766b8da3f2344038481e174c
John Leider <john@vuetifyjs.com>
2024-03-03 17:24:18 -0600

    fix(VoDialogSubheader): don't show title or text if not supplied

--- src/components/app/VoDialogSubheader.vue
+++ src/components/app/VoDialogSubheader.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-label class="font-weight-black">
+  <v-label v-if="title" class="font-weight-black">
     {{ title }}
   </v-label>
 
-  <div class="mb-3 text-caption text-medium-emphasis">
+  <div v-if="text" class="mb-3 text-caption text-medium-emphasis">
     {{ text }}
   </div>
 </template>
