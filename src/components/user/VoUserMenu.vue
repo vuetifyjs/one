34d81f169311ab7832a1fbd545ecb26e6def8b8d
John Leider <john@vuetifyjs.com>
2024-03-04 16:19:23 -0600

    chore(VoUserMenu): remove unused imports

--- src/components/user/VoUserMenu.vue
+++ src/components/user/VoUserMenu.vue
@@ -23,22 +23,5 @@
 </template>
 
 <script setup lang="ts">
-  // Utilities
-  import { shallowRef, watch } from 'vue'
-
-  // Stores
-  import { useAuthStore } from '@/store/auth'
-  import { useUserStore } from '@/store/user'
-
-  const auth = useAuthStore()
-  const user = useUserStore()
-  const social = shallowRef(!auth.user)
-
-  watch(() => user.syncSettings, (val, oldVal) => {
-    if (val && oldVal === false) social.value = true
-  })
-
-  watch(() => auth.user, val => {
-    if (val) social.value = false
-  })
+  //
 </script>
