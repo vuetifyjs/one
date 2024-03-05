af45b5f14c8893f7df7d298b91af4ba308e10eec
John Leider <john@vuetifyjs.com>
2024-03-05 09:55:43 -0600

    chore(user.dashboard): add page for testing auth callback

---
+++ dev/pages/user.dashboard.vue
@@ -0,0 +1,7 @@
+<template>
+  <Index />
+</template>
+
+<script lang="ts" setup>
+  import Index from './index.vue'
+</script>
--- dev/typed-router.d.ts
+++ dev/typed-router.d.ts
@@ -40,6 +40,7 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/user.dashboard': RouteRecordInfo<'/user.dashboard', '/user/dashboard', Record<never, never>, Record<never, never>>,
   }
 }
 
