485520c3495144fb371753d336a6a622d74f863d
John Leider <john@vuetifyjs.com>
2024-03-05 08:35:54 -0600

    fix(VoNotificationsTotalBadge): hide if notifications are off

--- src/components/notifications/VoNotificationsTotalBadge.vue
+++ src/components/notifications/VoNotificationsTotalBadge.vue
@@ -1,10 +1,10 @@
 <template>
   <v-badge
     :content="items"
+    :dot="!!auth.user"
     :model-value="items > 0"
     color="error"
     location="start top"
-    dot
   >
     <slot />
   </v-badge>
@@ -15,11 +15,15 @@
   import { computed } from 'vue'
 
   // Stores
+  import { useAuthStore } from '@/store/auth'
   import { useNotificationsStore } from '@/store/notifications'
+  import { useUserStore } from '@/store/user'
 
+  const auth = useAuthStore()
   const notifications = useNotificationsStore()
+  const user = useUserStore()
 
   const items = computed(() => {
-    return notifications.unread.length
+    return user.notifications.show ? notifications.unread.length : 0
   })
 </script>
