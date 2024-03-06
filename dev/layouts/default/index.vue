0d4db0a1d57395676edfe698a2c22576bd30ee1c
John Leider <john@vuetifyjs.com>
2024-03-06 13:16:58 -0600

    chore: move layout components into actual layout

--- dev/layouts/default/index.vue
+++ dev/layouts/default/index.vue
@@ -1,12 +1,50 @@
 <template>
   <v-app>
+    <VoNotificationsBanner />
+
+    <v-app-bar :image="settings.suit['app-bar']">
+      <template #prepend>
+        <router-link class="ps-2" to="/">
+          <v-img :src="image" width="200" />
+        </router-link>
+      </template>
+
+      <template #append>
+        <VoAuthBtn />
+      </template>
+    </v-app-bar>
+
+    <v-navigation-drawer :image="settings.suit['drawer']">
+      <v-list :items="items" item-props />
+    </v-navigation-drawer>
+
     <v-main>
       <router-view />
-
       <VoFooter app />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts" setup>
+  // Components
+  import { useTheme } from 'vuetify'
+
+  // Utilities
+  import { computed } from 'vue'
+
+  // Stores
+  import { useSettingsStore } from '../../../src/store/settings'
+  import { mdiHomeOutline, mdiViewDashboardOutline } from '@mdi/js'
+
+  const theme = useTheme()
+
+  const settings = useSettingsStore()
+
+  const items = [
+    { title: 'Home', to: '/', prependIcon: `svg:${mdiHomeOutline}` },
+    { title: 'Dashboard', to: '/user/dashboard/', prependIcon: `svg:${mdiViewDashboardOutline}` },
+  ]
+  const image = computed(() => {
+    return `https://vuetifyjs.b-cdn.net/docs/images/one/logos/vuetify-logo-one-${theme.current.value.dark ? 'dark' : 'light'}.png`
+  })
 </script>
--- dev/pages/index.vue
+++ dev/pages/index.vue
@@ -1,15 +1,7 @@
 <template>
-  <v-app-bar :image="settings.suit['app-bar']">
-    <template #append>
-      <VoAuthBtn />
-    </template>
-  </v-app-bar>
-  <v-navigation-drawer :image="settings.suit['drawer']" />
-
-  <v-container class="d-flex" />
+  <router-view />
 </template>
 
 <script lang="ts" setup>
-  import { useSettingsStore } from '../../src/store/settings'
-  const settings = useSettingsStore()
+  //
 </script>
