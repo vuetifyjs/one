6c08e5e78bdd59aa4719926f467f4d2364b87839
John Leider <john@vuetifyjs.com>
2024-03-05 20:13:58 -0600

    feat(VoAuthListItem): bootstrap component for install

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Components
 import VoAuthBtn from '@/components/auth/VoAuthBtn.vue'
+import VoAuthListItem from '@/components/auth/VoAuthListItem.vue'
 import VoFooter from '@/components/app/VoFooter.vue'
 
 // Types
@@ -17,6 +18,7 @@ export { useSettingsStore } from '@/store/settings'
 export function createOne () {
   function install (app: App) {
     app.component('VoAuthBtn', VoAuthBtn)
+    app.component('VoAuthListItem', VoAuthListItem)
     app.component('VoFooter', VoFooter)
   }
 
