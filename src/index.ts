dfb4f6510458afabbf1f132571a57fbc5c768550
John Leider <john@vuetifyjs.com>
2024-03-07 15:31:02 -0600

    chore: add auth dialog and notifications banner to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 // Components
 import VoAuthBtn from '@/components/auth/VoAuthBtn.vue'
+import VoAuthDialog from '@/components/auth/VoAuthDialog.vue'
+import VoNotificationsBanner from '@/components/notifications/VoNotificationsBanner.vue'
 import VoAuthListItem from '@/components/auth/VoAuthListItem.vue'
 import VoFooter from '@/components/app/VoFooter.vue'
 
@@ -18,7 +20,9 @@ export { useSettingsStore } from '@/store/settings'
 export function createOne () {
   function install (app: App) {
     app.component('VoAuthBtn', VoAuthBtn)
+    app.component('VoAuthDialog', VoAuthDialog)
     app.component('VoAuthListItem', VoAuthListItem)
+    app.component('VoNotificationsBanner', VoNotificationsBanner)
     app.component('VoFooter', VoFooter)
   }
 
