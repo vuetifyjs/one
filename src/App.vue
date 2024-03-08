2f65b23cb3720762277ad81223d16e26fd987cb2
John Leider <john@vuetifyjs.com>
2024-03-08 16:06:40 -0600

    chore(vercel): add analytics and speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <router-view />
+
+  <SpeedInsights />
 </template>
 
 <script lang="ts" setup>
-  //
+  import { inject } from '@vercel/analytics'
+  import { SpeedInsights } from '@vercel/speed-insights/vue'
+
+  inject()
 </script>
