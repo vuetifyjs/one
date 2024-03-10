003dffc92f6de18a6fec4a1c52e373c4013a11a2
John Leider <john@vuetifyjs.com>
2024-03-09 21:41:23 -0600

    refactor(dashboard): move into nested folder

---
+++ src/pages/user/dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    Dashboard
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  import { definePage } from 'vue-router/auto'
+
+  definePage({
+    meta: {
+      requiresAuth: true,
+    },
+  })
+</script>
