5b93f7db2d1429f61472254da48cc7f5f3d0e48b
John Leider <john@vuetifyjs.com>
2024-03-09 21:40:58 -0600

    chore(AppDrawer): add comments

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -11,15 +11,38 @@
 </template>
 
 <script lang="ts" setup>
+  // Composables
+  import { useAuthStore } from '@/stores/auth'
+
   // Utilities
+  import { computed } from 'vue'
   import { useSettingsStore } from '@/stores/settings'
 
   // Icons
-  import { mdiViewDashboardOutline } from '@mdi/js'
+  import { mdiBillboard, mdiViewDashboardOutline } from '@mdi/js'
 
   const settings = useSettingsStore()
 
-  const items = [
+  const auth = useAuthStore()
+
+  const links = [
     { title: 'Dashboard', to: '/user/dashboard/', prependIcon: `svg:${mdiViewDashboardOutline}` },
   ]
+
+  const admin = [
+    { type: 'subheader', class: 'mt-4', title: 'Admin' },
+    { type: 'divider', class: 'mb-2' },
+    {
+      title: 'Banners',
+      prependIcon: `svg:${mdiBillboard}`,
+      children: [
+        { title: 'List', to: '/banners/' },
+        { title: 'Create', to: '/banners/create' },
+      ],
+    },
+  ]
+
+  const items = computed(() => {
+    return (auth.user?.isAdmin) ? [...links, ...admin] : links
+  })
 </script>
