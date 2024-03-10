1e4dacdf8627e9c9bde62209808aac216f03cbcd
John Leider <john@vuetifyjs.com>
2024-03-09 21:38:43 -0600

    chore(route): add useParams function

--- lib/composables/route.ts
+++ lib/composables/route.ts
@@ -1,5 +1,6 @@
 // Utilities
-import { RouteParams, useRoute } from 'vue-router'
+import { RouteParams } from 'vue-router'
+import { useRoute } from 'vue-router/auto'
 import { computed } from 'vue'
 
 // https://github.com/vuejs/router/issues/1160#issuecomment-1055276586
@@ -8,5 +9,14 @@ export function useQuery<P extends RouteParams> () {
 
   return computed(
     () => route.query as P
+  )
+}
+
+// https://github.com/vuejs/router/issues/1160#issuecomment-1055276586
+export function useParams<P extends RouteParams> () {
+  const route = useRoute()
+
+  return computed(
+    () => route.params as P
   )
 }
