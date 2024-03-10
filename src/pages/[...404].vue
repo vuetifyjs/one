a06141d4f8a7ca934a5a78d39d22c8e44ad053af
John Leider <john@vuetifyjs.com>
2024-03-09 21:38:10 -0600

    feat: add 401 and 404 pages

---
+++ src/pages/[...404].vue
@@ -0,0 +1,42 @@
+<template>
+  <v-empty-state
+    color="primary"
+    icon="$vuetify-outline"
+    title="404 Not Found"
+  >
+    <template #media>
+      <v-icon color="medium-emphasis" size="256" />
+    </template>
+
+    <template #headline>
+      <div class="text-h1 mt-n6 mb-6">Oops!</div>
+    </template>
+
+    <template #text>
+      <p class="text-body-2 text-medium-emphasis py-4">
+        The page you are looking for does not exist. If you feel this is in error, please contact
+        <a class="text-white" href="mailto:hello@vuetifyjs.com?subject=Missing%20Page" target="_blank">hello@vuetifyjs.com</a>.
+      </p>
+    </template>
+
+    <template #actions>
+      <v-btn text="Go Back" width="150" @click="router.go(-1)" />
+    </template>
+  </v-empty-state>
+</template>
+
+<script lang="ts" setup>
+  // Composables
+  import { useRouter } from 'vue-router'
+
+  // Utilities
+  import { definePage } from 'vue-router/auto'
+
+  definePage({
+    meta: {
+      guest: true,
+    },
+  })
+
+  const router = useRouter()
+</script>
--- typed-router.d.ts
+++ typed-router.d.ts
@@ -19,6 +19,10 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
-    '/user.dashboard': RouteRecordInfo<'/user.dashboard', '/user/dashboard', Record<never, never>, Record<never, never>>,
+    '/[...404]': RouteRecordInfo<'/[...404]', '/:404(.*)', { 404: ParamValue<true> }, { 404: ParamValue<false> }>,
+    '/401': RouteRecordInfo<'/401', '/401', Record<never, never>, Record<never, never>>,
+    '/banners/': RouteRecordInfo<'/banners/', '/banners', Record<never, never>, Record<never, never>>,
+    '/banners/[id].edit': RouteRecordInfo<'/banners/[id].edit', '/banners/:id/edit', { id: ParamValue<true> }, { id: ParamValue<false> }>,
+    '/user/dashboard': RouteRecordInfo<'/user/dashboard', '/user/dashboard', Record<never, never>, Record<never, never>>,
   }
 }
