1aa083f6c0525fae5562632bc36e3463df28131f
John Leider <john@vuetifyjs.com>
2024-03-23 20:24:05 -0500

    docs(VoSubscriptionPerks): update perks information

--- lib/components/subscription/VoSubscriptionPerks.vue
+++ lib/components/subscription/VoSubscriptionPerks.vue
@@ -54,14 +54,15 @@
 
   const perks = [
     'Ad Free Experience on all Vuetify properties',
-    'Save and share code snippets with <a href="https://vbin.io/" target="_blank">VBin</a>',
+    'Save and share your playgrounds on <a href="https://play.vuetifyjs.com" target="_blank">VPlay</a>',
+    'Save and share code snippets on <a href="https://bin.vuetifyjs.com" target="_blank">VBin</a>',
     'Pinned Navigation Items and Rail drawer in <a href="https://vuetifyjs.com/" target="_blank">Documentation</a>',
     'Customize Navigation components with Page Suits',
     'Custom Vuetify One menu avatar',
   ]
   const upnext = [
+    'Early access to Vuetify Studio, a visual editor for Vuetify',
     'Special role in the <a href="https://discord.vuetifyjs.com/" target="_blank">Vuetify Discord</a>',
-    'Save and share your playgrounds with <a href="https://play.vuetifyjs.com" target="_blank">Vuetify Play</a>',
     'Receive a special label on GitHub created issues and pull requests',
   ]
 
