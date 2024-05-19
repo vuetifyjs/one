4ffddc3ce648fd0ab92d34ff72538140f93f4ab6
John Leider <john@vuetifyjs.com>
2024-05-19 16:39:39 -0500

    refactor(VoAuthCard): small design tweaks

--- lib/components/auth/VoAuthCard.vue
+++ lib/components/auth/VoAuthCard.vue
@@ -13,10 +13,10 @@
 
     <div class="text-center mb-6">
       <v-card-title class="text-h5 mb-1 text-md-h4">
-        <div class="font-weight-light mb-2">
+        <div class="text-h6 font-weight-light">
           {{ auth.lastLoginProvider() ? 'Welcome back to' : 'Login to' }}
         </div>
-        <span class="font-weight-bold">Vuetify One</span>
+        <span class="text-h3 font-weight-bold">Vuetify One</span>
       </v-card-title>
 
       <v-card-subtitle class="text-wrap">
