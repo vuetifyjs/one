37e03b0ae55953c5a44456f4c57daa5c67ad6561
John Leider <john@vuetifyjs.com>
2024-06-13 20:20:06 -0500

    chore(package.json): remove vercel

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,8 @@
 
     <router-view />
   </v-app>
-
-  <SpeedInsights />
 </template>
 
 <script lang="ts" setup>
-  // Utilities
-  import { inject } from '@vercel/analytics'
-  import { SpeedInsights } from '@vercel/speed-insights/vue'
-
-  inject()
+  //
 </script>
