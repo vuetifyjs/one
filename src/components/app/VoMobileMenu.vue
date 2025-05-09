6d2e3e3d9a6be228ae1283ffc7170899f4979bb9
abdelouahed oumoussa <abdelwahd.oumouss@gmail.com>
2025-05-09 01:02:49 +0100

    feat(VoAppBar): unify responsive behavior across all projects

---
+++ src/components/app/VoMobileMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-menu :close-on-content-click="false">
+    <template #activator="{ props }">
+      <v-btn v-bind="props" :icon="`svg:${mdiDotsVertical}`" />
+    </template>
+
+    <v-card class="pa-4">
+      <template v-if="slots.prepend">
+        <div class="mb-3">
+          <slot name="prepend" />
+        </div>
+      </template>
+
+      <template v-if="slots.title">
+        <v-divider v-if="slots.prepend" class="mb-3" />
+        <div class="text-h6 mb-3">
+          <slot name="title" />
+        </div>
+      </template>
+
+      <slot v-if="slots.default" name="default" />
+
+      <template v-if="slots.extension">
+        <div class="mt-2">
+          <slot name="extension" />
+        </div>
+      </template>
+
+      <template v-if="slots.append">
+        <v-divider class="my-3" />
+        <div class="mt-2">
+          <slot name="append" />
+        </div>
+      </template>
+    </v-card>
+  </v-menu>
+</template>
+
+<script lang="ts" setup>
+  import { mdiDotsVertical } from '@mdi/js';
+
+  const slots = defineSlots<{
+    default?(props: void): void;
+    prepend?(props: void): void;
+    title?(props: void): void;
+    extension?(props: void): void;
+    append?(props: void): void;
+  }>();
+</script>
