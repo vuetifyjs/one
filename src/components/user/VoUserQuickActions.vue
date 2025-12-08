3db4b6a83fc9f5d1054f8a4298a4aa6a64a1c134
John Leider <john@vuetifyjs.com>
2025-12-08 11:52:32 -0600

    feat(VoUserQuickActions): add new actions

--- src/components/user/VoUserQuickActions.vue
+++ src/components/user/VoUserQuickActions.vue
@@ -1,14 +1,16 @@
 <template>
   <v-expand-transition>
     <div v-if="user.one.quicklinks">
-      <div class="d-flex align-center justify-center pa-1 ga-1">
+      <div class="d-flex align-center justify-center py-2 px-6 ga-1 flex-wrap">
         <v-btn
+          v-for="action in actions"
+          :key="action.name"
           class="text-caption"
           :color="user.one.colors.primary"
           density="comfortable"
           flat
-          href="https://play.vuetifyjs.com"
-          icon="$vuetify-play"
+          :href="action.href"
+          :icon="action.icon"
           target="_blank"
         >
           <v-icon />
@@ -16,97 +18,7 @@
           <v-tooltip
             activator="parent"
             location="top"
-            text="Vuetify Play"
-          />
-        </v-btn>
-
-        <v-btn
-          class="text-caption"
-          :color="user.one.colors.primary"
-          density="comfortable"
-          flat
-          href="https://bin.vuetifyjs.com"
-          icon="$vuetify-bin"
-          target="_blank"
-        >
-          <v-icon />
-
-          <v-tooltip
-            activator="parent"
-            location="top"
-            text="Vuetify Bin"
-          />
-        </v-btn>
-
-        <v-btn
-          class="text-caption"
-          :color="user.one.colors.primary"
-          density="comfortable"
-          flat
-          href="https://studio.vuetifyjs.com"
-          icon="$vuetify-studio"
-          target="_blank"
-        >
-          <v-icon />
-
-          <v-tooltip
-            activator="parent"
-            location="top"
-            text="Vuetify Studio"
-          />
-        </v-btn>
-
-        <v-btn
-          class="text-caption"
-          :color="user.one.colors.primary"
-          density="comfortable"
-          flat
-          href="https://snips.vuetifyjs.com"
-          icon="$vuetify-snips"
-          target="_blank"
-        >
-          <v-icon />
-
-          <v-tooltip
-            activator="parent"
-            location="top"
-            text="Vuetify Snips"
-          />
-        </v-btn>
-
-        <v-btn
-          class="text-caption"
-          :color="user.one.colors.primary"
-          density="comfortable"
-          flat
-          href="https://link.vuetifyjs.com"
-          icon="$vuetify-link"
-          target="_blank"
-        >
-          <v-icon />
-
-          <v-tooltip
-            activator="parent"
-            location="top"
-            text="Vuetify Link"
-          />
-        </v-btn>
-
-        <v-btn
-          class="text-caption"
-          :color="user.one.colors.primary"
-          density="comfortable"
-          flat
-          href="https://issues.vuetifyjs.com"
-          icon="$vuetify-issues"
-          target="_blank"
-        >
-          <v-icon />
-
-          <v-tooltip
-            activator="parent"
-            location="top"
-            text="Vuetify Issues"
+            :text="action.name"
           />
         </v-btn>
       </div>
@@ -116,4 +28,42 @@
 
 <script lang="ts" setup>
   const user = useUserStore()
+
+  const actions = [
+    {
+      name: 'Vuetify Play',
+      href: 'https://play.vuetifyjs.com',
+      icon: '$vuetify-play',
+    },
+    {
+      name: 'Vuetify Bin',
+      href: 'https://bin.vuetifyjs.com',
+      icon: '$vuetify-bin',
+    },
+    {
+      name: 'Vuetify Studio',
+      href: 'https://studio.vuetifyjs.com',
+      icon: '$vuetify-studio',
+    },
+    {
+      name: 'Vuetify Snips',
+      href: 'https://snips.vuetifyjs.com',
+      icon: '$vuetify-snips',
+    },
+    {
+      name: 'Vuetify Link',
+      href: 'https://link.vuetifyjs.com',
+      icon: '$vuetify-link',
+    },
+    {
+      name: 'Vuetify Issues',
+      href: 'https://issues.vuetifyjs.com',
+      icon: '$vuetify-issues',
+    },
+    {
+      name: 'Vuetify Store',
+      href: 'https://store.vuetifyjs.com',
+      icon: '$vuetify-store',
+    },
+  ]
 </script>
